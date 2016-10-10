31cc9e895b12ec05e73e0f4e4ab91f751415c0db
Arpit <srivastava.arpit31@gmail.com>
2016-10-10 01:13:12 -0700

    Name calculator app using angular js

--- fullstack-course/app.js
+++ fullstack-course/app.js
@@ -1,4 +1,28 @@
 (function (){
+'use strict';
 
-angular.module('myFirstApp', []);  
+angular.module('NameCalculator', [])
+
+.controller('NameCalculatorController', function($scope) {
+  $scope.name = "";
+  $scope.totalVal = 0;
+
+  $scope.displayNumeric = function ()
+  {
+    var totalNameValue = calculateNameString($scope.name);
+    $scope.totalVal = totalNameValue;
+  };
+
+  function calculateNameString(string){
+    var totalStringValue = 0;
+    for(var i=0 ; i < string.length; i++)
+    {
+      totalStringValue += string.charCodeAt(i);
+    }
+
+    return totalStringValue;
+  }
+
+});
+
 })();
